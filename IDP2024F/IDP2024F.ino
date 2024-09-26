90e7b785f648d574ad0356b5c51d7ebff861ed0f
EliasSharonov <eliasc23@vt.edu>
2024-09-26 14:17:52 -0400

    Added closed-loop control

--- IDP2024F/IDP2024F.ino
+++ IDP2024F/IDP2024F.ino
@@ -4,11 +4,16 @@
 
 // Declare other constants here
 #define FET_PERIOD 10
-#define DIVIDER_CONSTANT 2
+#define DIVIDER_CONSTANT 4
+#define IDEAL_DUTY_CYCLE 0.53
+#define DUTY_CYCLE_CONSTANT 0.01
 
 
 // Variables belong here
 float dutyCycle;
+float dutyCycleError;
+int rawOutVoltage;
+float outVoltage;
 
 
 
@@ -19,10 +24,13 @@ void setup() {
 }
 
 void loop() {
-  int inputVoltage = analogRead(VINREG_PIN);
-  inputVoltage = (inputVoltage / DIVIDER_CONSTANT) / 1024.0;
+  rawOutVoltage = analogRead(VINREG_PIN);
+  outVoltage = ((float)rawOutVoltage / DIVIDER_CONSTANT) / 1024.0;
 
-  dutyCycle = 1 - (inputVoltage / 10.7);
+
+  
+  dutyCycleError = IDEAL_DUTY_CYCLE - (1.0 - (5.0 / outVoltage));
+  dutyCycle = IDEAL_DUTY_CYCLE - DUTY_CYCLE_CONSTANT * dutyCycleError;
   voltageRegulation(dutyCycle);
 }
 
