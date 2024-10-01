cd6701cf68e8b785e0d0f2a290f2937cfea0dcc2
smkcarmack <smkcarmack@gmail.com>
2024-10-01 12:01:40 -0400

    fhfyh

--- IDP2024F/IDP2024F.ino
+++ IDP2024F/IDP2024F.ino
@@ -3,19 +3,19 @@
 #define VINREG_PIN 9
 
 // Declare other constants here
-#define FET_PERIOD 10
+#define FET_PERIOD 5
 #define DIVIDER_CONSTANT 4
 #define IDEAL_DUTY_CYCLE 0.53
 #define DUTY_CYCLE_CONSTANT 0.01
+#define IDEAL_INPUT_VOLTAGE 9
 
 
 // Variables belong here
-float dutyCycle;
-float dutyCycleError;
 int rawOutVoltage;
 float outVoltage;
-
-
+float integral = 0;
+float previousError = 0;
+unsigned long lastTime = 0;
 
 
 void setup() {
@@ -27,17 +27,32 @@ void loop() {
   rawOutVoltage = analogRead(VINREG_PIN);
   outVoltage = ((float)rawOutVoltage / DIVIDER_CONSTANT) / 1024.0;
 
-
-  
-  dutyCycleError = IDEAL_DUTY_CYCLE - (1.0 - (5.0 / outVoltage));
-  dutyCycle = IDEAL_DUTY_CYCLE - DUTY_CYCLE_CONSTANT * dutyCycleError;
-  voltageRegulation(dutyCycle);
+  //voltageRegulation(PID_Control(outVoltage, 0.05, 0.05, 0.05));
+  while (1) {
+  voltageRegulation(0.53);
+  }
 }
+
+
+
 
 void voltageRegulation(float dutyCycle) {
   digitalWrite(PWMOUT_PIN, HIGH);
   delayMicroseconds(FET_PERIOD * dutyCycle);
   digitalWrite(PWMOUT_PIN, LOW);
   delayMicroseconds(FET_PERIOD * (1 - dutyCycle));
+}
 
+float PID_Control(float outVoltage, float P, float I, float D) {
+    float error = IDEAL_INPUT_VOLTAGE - outVoltage;
+    float dt = millis();
+    lastTime = dt;
+    dt = millis() - lastTime;
+    
+    integral += error * dt;
+    float derivative = (error - previousError) / dt;
+    float output = (P * error) + (I * integral) + (D * derivative);
+    previousError = error;
+
+    return output;
 }
