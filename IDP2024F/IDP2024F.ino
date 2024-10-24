1b5093d22f85275c93314d8d3c4e90658095f305
EliasSharonov <eliasc23@vt.edu>
2024-10-24 16:13:32 -0400

    Adjusted the code to use battery voltage as reference

--- IDP2024F/IDP2024F.ino
+++ IDP2024F/IDP2024F.ino
@@ -1,16 +1,20 @@
+#include "timer.h"
+
 // Declare pins here
 #define PWMOUT_PIN 10  // PB2
 #define VINREG_PIN A0
+#define BATTERY_PIN A1
 
 // Declare other constants here
 #define FET_PERIOD 10 //microseconds
 #define DIVIDER_CONSTANT 11 // Approximately
 #define IDEAL_DUTY_CYCLE 0.53
 #define DUTY_CYCLE_CONSTANT 0.01
-#define IDEAL_INPUT_VOLTAGE 10.7
+#define IDEAL_INPUT_VOLTAGE 10.0
 
 // Variables
 float outVoltage;
+float batteryVoltage;
 float dutyCycle = IDEAL_DUTY_CYCLE;
 
 // PID variables
@@ -25,13 +29,13 @@ unsigned long lastVoltageReadTime = 0;
 unsigned long lastPIDTime = 0;
 
 // PID constants
-#define P 0.001
-#define I 0.0
-#define D 0
+#define kP 0.01
+#define kI 0.1
+#define kD 0.0
 
 void setup() {
   pinMode(PWMOUT_PIN, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(BATTERY_PIN, INPUT);
   pinMode(VINREG_PIN, INPUT);
 
   noInterrupts();
@@ -47,15 +51,17 @@ void setup() {
   lastTime = micros();
   lastVoltageReadTime = millis();
   lastPIDTime = millis();
+  Serial.begin(9600);
 }
 
 void loop() {
-  unsigned long currentTime = millis();
+  unsigned long currentTime = micros();
 
   updatePWM();
 
   if (currentTime - lastVoltageReadTime >= 10) {
-    readVoltage();
+    readOutVoltage();
+    readBatteryVoltage();
     lastVoltageReadTime = currentTime;
   }
 
@@ -69,8 +75,12 @@ void updatePWM() {
   OCR1B = (uint16_t)(dutyCycle * ICR1);
 }
 
-void readVoltage() {
+void readOutVoltage() {
   outVoltage = ((float)analogRead(VINREG_PIN) * DIVIDER_CONSTANT) / 203.0;
+}
+
+void readBatteryVoltage() {
+  batteryVoltage = ((float)analogRead(BATTERY_PIN) * DIVIDER_CONSTANT) / 203.0;
 }
 
 void adjustPID() {
@@ -78,12 +88,12 @@ void adjustPID() {
   dt = (currentMicros - lastTime) / 1000000.0;
   lastTime = currentMicros;
 
-  error = IDEAL_INPUT_VOLTAGE - outVoltage;
+  error = batteryVoltage + 1.5 - outVoltage;
+    Serial.println(error);
   integral += error * dt;
   derivative = (error - previousError) / dt;
 
-  dutyCycle = 0.53 + P * error + I * integral;
-  // 0.1*((P * error) + (I * integral) + (D * derivative));
+  dutyCycle = 0.53 + ((kP * error) + (kI * integral) + (kD * derivative));
 
   if (dutyCycle > 1) dutyCycle = 1;
   if (dutyCycle < 0) dutyCycle = 0;
