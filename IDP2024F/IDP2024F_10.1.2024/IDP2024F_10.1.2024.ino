8e085e5836f126b97b7fe65be633dc7b83c27cb8
EliasSharonov <eliasc23@vt.edu>
2024-11-17 17:21:42 -0500

    Sleep Mode???

--- IDP2024F/IDP2024F_10.1.2024/IDP2024F_10.1.2024.ino
+++ IDP2024F/IDP2024F_10.1.2024/IDP2024F_10.1.2024.ino
@@ -1,66 +1,111 @@
 // Declare pins here
-#define PWMOUT_PIN 10
-#define VINREG_PIN 9
+#define PWMOUT_PIN 10  // PB2
+#define VINREG_PIN A0
+#define BATTERY_PIN A1
+#define THERMISTOR_PIN A2
 
 // Declare other constants here
-#define FET_PERIOD 5
-#define DIVIDER_CONSTANT 4
-#define IDEAL_DUTY_CYCLE 0.53
-#define DUTY_CYCLE_CONSTANT 0.01
-#define IDEAL_INPUT_VOLTAGE 9
+#define FET_PERIOD 10 //microseconds
+#define DIVIDER_CONSTANT 11 // Approximately
+#define IDEAL_DUTY_CYCLE 0.6 // Increased baseline for higher duty cycle start
+#define MAX_OUT_VOLTAGE 11.5 // Maximum output voltage limit
 
+#define THERMISTOR_NOMINAL 10000.0
+#define TEMPERATURE_NOMINAL 25.0
+#define B_COEFFICIENT 3350.0
+#define SERIES_RESISTOR 10000.0
 
-// Variables belong here
-int rawOutVoltage;
+// Variables
 float outVoltage;
+float batteryVoltage;
+float temperature;
+
+float dutyCycle = IDEAL_DUTY_CYCLE;
+
+// PID variables
+float error = 0;
 float integral = 0;
 float previousError = 0;
-float dutyCycle = 0;
+float dt = 0;
+float derivative =  0;
+
 unsigned long lastTime = 0;
-unsigned long timeStamp = 0; // Merge with lastTime???
+unsigned long lastVoltageReadTime = 0;
+unsigned long lastPIDTime = 0;
+
+
+
+// PID constants
+#define kP 0.03  // Slightly increased proportional gain
+#define kI 0.05  // Increased integral gain to push the duty cycle higher
+#define kD 0.0
 
 void setup() {
   pinMode(PWMOUT_PIN, OUTPUT);
+  pinMode(BATTERY_PIN, INPUT);
+  pinMode(VINREG_PIN, INPUT);
+
+  noInterrupts();
+
+  TCCR1A = (1 << COM1B1) | (1 << WGM11); 
+  TCCR1B = (1 << WGM12) | (1 << WGM13) | (1 << CS10);
+
+  ICR1 = 160;  
+  OCR1B = (uint16_t)(IDEAL_DUTY_CYCLE * ICR1);  
+
+  interrupts();
+
+  lastTime = micros();
+  lastVoltageReadTime = millis();
+  lastPIDTime = millis();
   Serial.begin(9600);
 }
 
 void loop() {
-  timeStamp = micros();
-  digitalWrite(PWMOUT_PIN, HIGH);
-  rawOutVoltage = analogRead(VINREG_PIN);
+  unsigned long currentTime = micros();
 
-  while (timeStamp - micros() < FET_PERIOD * dutyCycle) {
-    delayMicroseconds(1);
+  OCR1B = (uint16_t)(dutyCycle * ICR1);
+
+  if (currentTime - lastVoltageReadTime >= 10) {
+    outVoltage = ((float)analogRead(VINREG_PIN) * DIVIDER_CONSTANT) / 203.0;
+    batteryVoltage = ((float)analogRead(BATTERY_PIN) * DIVIDER_CONSTANT) / 203.0;
+    lastVoltageReadTime = currentTime;
   }
-  timeStamp = micros();
-  outVoltage = ((float)rawOutVoltage / DIVIDER_CONSTANT) / 1024.0;
-  digitalWrite(PWMOUT_PIN, LOW);
-  while (timeStamp - micros() < FET_PERIOD * (1 - dutyCycle)) {
-    delayMicroseconds(1);
+
+  if (currentTime - lastPIDTime >= 10) {
+    adjustPID();
+    lastPIDTime = currentTime;
   }
-  dutyCycle = 0.53;
+
+    temperature = log(SERIES_RESISTOR / (1023.0 / (float)analogRead(THERMISTOR_PIN) - 1.0));
+    Serial.print("Debug resistance: ");
+    Serial.println(temperature);
+
+    temperature = (1.009249522e-03 + 2.378405444e-04 * temperature + 2.019202697e-07 * temperature * temperature * temperature) - 273.15;
+    // Debugging
+    Serial.print("Temperature: ");
+    Serial.println(temperature);
+
+
+  delay(500);
 }
 
+void adjustPID() {
+  unsigned long currentMicros = micros();
+  dt = (currentMicros - lastTime) / 1000000.0;
+  lastTime = currentMicros;
 
+  error = batteryVoltage + 1.0 - outVoltage;
 
+  integral += error * dt;  // Accumulate integral without clamping
+  derivative = (error - previousError) / dt;
 
-void voltageRegulation(float dutyCycle) {
-  digitalWrite(PWMOUT_PIN, HIGH);
-  delayMicroseconds(FET_PERIOD * dutyCycle);
-  digitalWrite(PWMOUT_PIN, LOW);
-  delayMicroseconds(FET_PERIOD * (1 - dutyCycle));
-}
+  dutyCycle = IDEAL_DUTY_CYCLE + (kP * error) + (kI * integral) + (kD * derivative);
 
-float PID_Control(float outVoltage, float P, float I, float D) {
-    float error = IDEAL_INPUT_VOLTAGE - outVoltage;
-    float dt = micros();
-    lastTime = dt;
-    dt = micros() - lastTime;
-    
-    integral += error * dt;
-    float derivative = (error - previousError) / dt;
-    float output = (P * error) + (I * integral) + (D * derivative);
-    previousError = error;
+  // Clamp the duty cycle between 0 and 1
+  if (dutyCycle > 1) dutyCycle = 1;
+  if (dutyCycle < 0) dutyCycle = 0;
 
-    return output;
+  // Debugging information
+  previousError = error;
 }
--- IDP2024F/IDP2024F_10.28.2024/IDP2024F_10.28.2024.ino
+++ IDP2024F/IDP2024F_10.28.2024/IDP2024F_10.28.2024.ino
@@ -1,27 +1,23 @@
 
-
 // Declare pins here
 #define PWMOUT_PIN 10  // PB2
 #define VINREG_PIN A0
 #define BATTERY_PIN A1
-#define THERMISTOR_PIN A2 // Thermistor pin
+#define THERMISTOR_PIN A2
 
 // Declare other constants here
 #define FET_PERIOD 10 //microseconds
 #define DIVIDER_CONSTANT 11 // Approximately
 #define IDEAL_DUTY_CYCLE 0.6 // Increased baseline for higher duty cycle start
 #define MAX_OUT_VOLTAGE 11.5 // Maximum output voltage limit
+#define THERMISTOR_SERIAL_RESISTANCE 10000.0
+#define VOLTAGE_THRESHOLD 3.3
+#define WAKE_UP_INTERVAL 30000 // (ms)
 
-#define THERMISTOR_NOMINAL 10000.0
-#define TEMPERATURE_NOMINAL 25.0
-#define B_COEFFICIENT 3350.0
-#define SERIES_RESISTOR 10000.0
-
-// General variables
+// Variables
 float outVoltage;
 float batteryVoltage;
-float temperature;
-
+int thermistorVoltage;
 float dutyCycle = IDEAL_DUTY_CYCLE;
 
 // PID variables
@@ -30,15 +26,26 @@ float integral = 0;
 float previousError = 0;
 float dt = 0;
 float derivative =  0;
+float temperature;
+volatile bool wdtWakeUp = false;
+volatile uint8_t wdtInterruptCount = 0;
 
 unsigned long lastTime = 0;
 unsigned long lastVoltageReadTime = 0;
 unsigned long lastPIDTime = 0;
 
+// Temperature constants
+#define TEMPERATURE_C1 1.009249522e-03
+#define TEMPERATURE_C2 378405444e-04
+#define TEMPERATURE_C3 2.019202697e-07
+
 // PID constants
-#define kP 0.03  // Slightly increased proportional gain
+#define kP 0.01  // Slightly increased proportional gain
 #define kI 0.05  // Increased integral gain to push the duty cycle higher
 #define kD 0.0
+
+
+// SETUP
 
 void setup() {
   pinMode(PWMOUT_PIN, OUTPUT);
@@ -46,6 +53,11 @@ void setup() {
   pinMode(VINREG_PIN, INPUT);
 
   noInterrupts();
+
+  MCUSR &= ~(1 << WDRF);
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+  WDTCSR = (1 << WDP3) | (1 << WDP0); // Set timeout to 8 seconds
+  WDTCSR |= (1 << WDIE);
 
   TCCR1A = (1 << COM1B1) | (1 << WGM11); 
   TCCR1B = (1 << WGM12) | (1 << WGM13) | (1 << CS10);
@@ -61,34 +73,69 @@ void setup() {
   Serial.begin(9600);
 }
 
+
+// PROGRAM LOOP
+
 void loop() {
   unsigned long currentTime = micros();
 
+  // Sleep mode control
+  batteryVoltage = ((float)analogRead(BATTERY_PIN) * DIVIDER_CONSTANT) / 203.0;
+  if ((batteryVoltage <= VOLTAGE_THRESHOLD)) {
+    Serial.println("Sleep Mode"); 
+    enterSleepLoop();
+  }
+
+  // Duty cycle output
   OCR1B = (uint16_t)(dutyCycle * ICR1);
 
+  // Voltage reading
   if (currentTime - lastVoltageReadTime >= 10) {
     outVoltage = ((float)analogRead(VINREG_PIN) * DIVIDER_CONSTANT) / 203.0;
     batteryVoltage = ((float)analogRead(BATTERY_PIN) * DIVIDER_CONSTANT) / 203.0;
-
-
-
-
-    temperature = log(SERIES_RESISTOR * (1023.0 / (float)analogRead(THERMISTOR_PIN) - 1.0));
-    Serial.print("Debug resistance: ");
-    Serial.println(temperature);
-
-    temperature = (1.009249522e-03 + 2.378405444e-04 * temperature + 2.019202697e-07 * temperature * temperature * temperature) - 273.15;
-    // Debugging
-    Serial.print("Temperature: ");
-    Serial.println(temperature);
-
     lastVoltageReadTime = currentTime;
   }
+  thermistorVoltage = analogRead(THERMISTOR_PIN);
+  convertTernperature();
 
+  // Duty cycle adjustment
   if (currentTime - lastPIDTime >= 10) {
     adjustPID();
     lastPIDTime = currentTime;
   }
+
+  delay(500);
+}
+
+
+
+
+
+
+// HELPER FUNCTIONS
+
+void convertTernperature() {
+  temperature = THERMISTOR_SERIAL_RESISTANCE * (1023.0 / (float)thermistorVoltage - 1.0);
+  temperature = log(temperature);
+  temperature = (1.0 / (TEMPERATURE_C1 + TEMPERATURE_C2*temperature + TEMPERATURE_C3*temperature*temperature*temperature));
+  temperature = temperature - 273.15;
+  temperature = (temperature * 9.0)/ 5.0 + 32.0; 
+
+}
+
+
+void debugInfo() {
+  Serial.print("Temperature: "); 
+  Serial.print(temperature);
+  Serial.println(" F"); 
+  Serial.print("Duty Cycle: "); 
+  Serial.println(dutyCycle);
+  Serial.print("Out Voltage: "); 
+  Serial.print(outVoltage);
+  Serial.println(" V"); 
+  Serial.print("Battery voltage: "); 
+  Serial.print(batteryVoltage);
+  Serial.println(" V"); 
 }
 
 void adjustPID() {
@@ -103,10 +150,43 @@ void adjustPID() {
 
   dutyCycle = IDEAL_DUTY_CYCLE + (kP * error) + (kI * integral) + (kD * derivative);
 
-  // Clamp the duty cycle between 0 and 1
   if (dutyCycle > 1) dutyCycle = 1;
   if (dutyCycle < 0) dutyCycle = 0;
 
-  // Debugging information
   previousError = error;
+}
+
+
+// SLEEP LOOP
+
+void enterSleepLoop() {
+  SMCR = (1 << SM1) | (1 << SE);
+  while (true) {
+    SMCR |= (1 << SE);
+    do {
+    __asm__ __volatile__ ( "sleep" "\n\t" :: );
+    } while(0);
+    SMCR &= ~(1 << SE);
+
+    if (wdtWakeUp) {
+      wdtWakeUp = false;
+
+      wdtInterruptCount++;
+
+      if (wdtInterruptCount >= 4) { // 8*4 = 32 seconds
+        wdtInterruptCount = 0;
+        batteryVoltage = ((float)analogRead(BATTERY_PIN) * DIVIDER_CONSTANT) / 203.0;
+        if (batteryVoltage > VOLTAGE_THRESHOLD) {
+          return;
+        }
+      }
+    }
+  }
+}
+
+
+// Interrupt control
+ISR(WDT_vect) {
+  Serial.println("Awake Mode"); 
+  wdtWakeUp = true;
 }
